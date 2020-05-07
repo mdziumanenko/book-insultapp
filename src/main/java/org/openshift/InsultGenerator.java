aab92747f2e302f8fe3efdfd0f454ede3d871d58
Maxim Dziumanenko <dziumanenko@gmail.com>
2020-05-07 20:29:32 +0300

    Add db support

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -19,16 +19,14 @@ public class InsultGenerator {
             Connection connection = DriverManager.getConnection(databaseURL, username,
             password);
             if (connection != null) {
-                String SQL = "select a.string AS first, b.string AS second, c.string AS noun
-                from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
+                String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
                 Statement stmt = connection.createStatement();
                 ResultSet rs = stmt.executeQuery(SQL);
                 while (rs.next()) {
                     if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
                         article = "a";
                     }
-                    theInsult = String.format("Thou art %s %s %s %s!", article,
-                        rs.getString("first"), rs.getString("second"), rs.getString("noun"));
+                    theInsult = String.format("Thou art %s %s %s %s!", article, rs.getString("first"), rs.getString("second"), rs.getString("noun"));
                 }
                 rs.close();
                 connection.close();
